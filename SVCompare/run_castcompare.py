2a2aad36915ac235ff3ac0c5b1625af3e2ad5647
Damian Manda <damian.manda@noaa.gov>
2013-06-29 12:56:04 -0800

    Added dialog for confirmation of same color, os compatibility

--- SVCompare/run_castcompare.py
+++ SVCompare/run_castcompare.py
@@ -1,7 +1,9 @@
 import wx
 import wx.xrc
+import os
 from cast_compare_frm import frm_compare
-import sv_compare
+if os.name != 'nt':
+    import sv_compare
 import os.path
 
 #size 550x641
@@ -18,8 +20,13 @@ class CompareFrame(frm_compare):
         path = os.path.split(self.fp_ref.GetPath())[0]
         ref_color = self.ch_color_ref.GetStringSelection()
         comp_color = self.ch_color_comp.GetStringSelection()
-        save_output = self.chk_save_output.GetValue()
+        if ref_color == comp_color:
+            result = wx.MessageDialog(self, 'Do you really want both colors to be the same?', 
+                style=wx.YES_NO | wx.ICON_QUESTION).ShowModal()
+            if result == wx.ID_NO:
+                return
 
+        save_output = self.chk_save_output.GetValue()
         results = sv_compare.compare_files(self.fp_ref.GetPath(),
             self.fp_comp.GetPath(), ref_color, comp_color,
             float(self.txt_lat.GetValue()), save_output)
